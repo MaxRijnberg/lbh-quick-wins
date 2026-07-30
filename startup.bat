479b5f9dc9c8d01d2a2639eb4896cc5c161e1acc
MaxRijnberg <139171637+MaxRijnberg@users.noreply.github.com>
2026-07-30 09:39:48 +0200

    Change localhost URL in the comment from 80 to 8501
    Updated the URL to open after Docker Compose starts from localhost:80 to localhost:8501.

--- startup.bat
+++ startup.bat
@@ -4,7 +4,7 @@ setlocal
 REM Start docker compose (blocking)
 docker compose up --build --force-recreate
 
-REM Open localhost:80 after compose starts/ends
+REM Open localhost:8501 after compose starts/ends
 start "" "http://localhost:8501"
 
 endlocal
